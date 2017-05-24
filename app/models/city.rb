8cd4c48116ca31aae673f13f6e5ee1b9724a3280
Jose A. Campos <jjoseccampos@gmail.com>
2017-05-24 18:08:13 -0400

    added shrine but not yet working

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,9 @@
 class City < ApplicationRecord
+  include ImageUploader[:image]
+
+  validates :city_name, presence: true
+  validates :state, presence: true
+  validates :us_city, presence: true
+  validates :population, presence: true
+  validates :one_hundred_pct_by, presence: true
 end
