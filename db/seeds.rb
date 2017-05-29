a795f3e7ce96194f57255ec0646803a29df22bda
Jose A. Campos <jjoseccampos@gmail.com>
2017-05-29 14:03:11 -0400

    seeds working and rendering pics

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+City.destroy_all
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'EnergyCSV.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  g = City.new
+  g.city_name = row['city_name']
+  g.state = row['state']
+  g.us_city = row['us_city']
+  g.population = row['population']
+  g.one_hundred_pct_by = row['one_hundred_pct_by']
+  g.co2_emissions_perannum = row['co2_emissions_perannum']
+  g.wind = row['wind']
+  g.solar = row['solar']
+  g.other_energy_source = row['other_energy_source']
+  g.save
+
+  puts "#{g.city_name} saved"
+end
