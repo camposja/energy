5deec860f891eb37982da43af88ba57ee1213ad8
Jose A. Campos <jjoseccampos@gmail.com>
2017-05-29 13:21:36 -0400

    removed validation to require city name for seed

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,7 +1,6 @@
 class City < ApplicationRecord
   include ImageUploader[:image]
 
-  validates :city_name, presence: true
   validates :state, presence: true
   validates :us_city, presence: true
   validates :population, presence: true
